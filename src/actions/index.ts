72dc0f27e1f9fd19372ff330f47a64a3d2b73656
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2024-02-23 14:18:18 +0100

    First commit

---
+++ src/actions/index.ts
@@ -0,0 +1,50 @@
+"use server";
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
+export async function editSnippet(id: number, code: string) {
+  console.log(id, code);
+  await db.snippet.update({
+    where: { id },
+    data: { code },
+  });
+
+  redirect(`/snippets/${id}`);
+}
+
+export async function deleteSnippet(id: number) {
+  await db.snippet.delete({ where: { id } });
+  redirect("/");
+}
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    // Check the user's inputs and make sure there are valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+      return { message: "Title must be at least 3 characters long" };
+    }
+    if (typeof code !== "string" || code.length < 10) {
+      return { message: "Code must be at least 10 characters long" };
+    }
+
+    // Create a new record in the database
+    await db.snippet.create({
+      data: { title, code },
+    });
+  } catch (error: unknown) {
+    // If there's an error, return the error message
+    if (error instanceof Error) {
+      return { message: error.message };
+    } else {
+      return { message: "An unknown error occurred" };
+    }
+  }
+  // Redirect the user back to the route route
+  redirect("/");
+}
---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function SnippetNotFound() {
+  return (
+    <div>
+      <h1 className="text-xl bold">We couldn't found it...</h1>
+    </div>
+  );
+}
